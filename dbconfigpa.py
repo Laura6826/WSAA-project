0eeecf09f63c2d331c9022e6ef261f11a08a58c7
Laura Lyons <maxwell6826@yahoo.co.uk>
2025-05-13 23:52:56 +0100

    dbconfigpa

---
+++ dbconfigpa.py
@@ -0,0 +1,10 @@
+# WSAA-project: Web Services and Applications.
+# Configuration file for MySQL database connection.
+# Author: Laura Lyons
+
+mysql = {
+    'host':"lauralyons1982.mysql.eu.pythonanywhere-services.com",
+    'user':"lauralyons1982",
+    'password':"mysqlpassword6826",
+    'database':"lauralyons1982$CarParks",
+}
